4523ea8f60cc897fb74fc7b300470bbef6898112
Unripper1 <dmharitoh@gmail.com>
2020-09-13 12:50:07 +0300

    1 finish

--- src/Dog.java
+++ src/Dog.java
@@ -1,6 +1,7 @@
 import java.util.LinkedList;
+import java.util.Scanner;
 
-public abstract class Dog {
+public class Dog {
     protected String name;
     protected int age;
     Dog(String n, int a){
@@ -28,15 +29,17 @@ public abstract class Dog {
 }
 
 class DogKennel {
-    void plus(LinkedList a){
-
+    void plus(LinkedList a, int age, String name){
+        Dog d=new Dog(name,age);
+        a.add(d);
     }
     public static void main(String[] args){
         int n=10;
         LinkedList<Dog> dogs=new LinkedList<>();
+        DogKennel ken=new DogKennel();
+        ken.plus(dogs,12,"ben");
+        System.out.println(dogs.get(0).ToString());
 
-        //Dog husky = new Husky();
-        //husky.setName("SK");
-        //husky.showName();
     }
+
 }
